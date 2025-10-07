7c28430ba29855a6408a0af89cca446fca20ff4e
Vincent <vincentz92@nycstudents.net>
2025-10-07 10:17:13 -0400

    Update database_viewer.py

--- database_viewer.py
+++ database_viewer.py
@@ -21,49 +21,53 @@ movies = open("./movies.json", encoding="utf8")
 data = json.load(movies)
 
 #File 1
-""" for i in range(len(data)):
-    print(data[i]['title']) """
+for i in range(len(data)):
+    print(data[i]['title'])
 
 #File 2
-""" release = input("Input a year.")
-release = int(release)
-for i in range(len(data)):
-    if (data[i]['year']) > release:
-        print(data[i]['title']) """
-
+def releasefinder(release):
+    release = int(release)
+    for i in range(len(data)):
+        if (data[i]['year']) > release:
+            print(data[i]['title'])
+releasefinder(1980)
 #File 3
-""" release = input("Input a year.")
-cutoff = input("Input another year, larger than the first one.")
-release = int(release)
-cutoff = int(cutoff)
-print(f"The movies released between {release} and {cutoff} are:")
-for i in range(len(data)):
-    if data[i]['year'] > release and data[i]['year'] < cutoff:
-        print(data[i]['title']) """
-
+def rangefinder(release, cutoff):
+    release = int(release)
+    cutoff = int(cutoff)
+    print(f"The movies released between {release} and {cutoff} are:")
+    for i in range(len(data)):
+        if data[i]['year'] > release and data[i]['year'] < cutoff:
+            print(data[i]['title'])
+rangefinder(1980, 1985)
 #File 4
-""" year = input("Input a year.")
-year = int(year)
-print(f"The movies released in {year} are:")
-for i in range(len(data)):
-    if data[i]['year'] == year:
-        print(data[i]['title']) """
-
+def movie_year_finder(year):
+    year = int(year)
+    print(f"The movies released in {year} are:")
+    for i in range(len(data)):
+        if data[i]['year'] == year:
+            print(data[i]['title'])
+movie_year_finder(1980)
 #File 5
-""" movies_found = 0
-name = input("Search a movie.")
-print(f"Here are the results for '{name}':")
-for i in range(len(data)):
-    if data[i]['title'] == name:
-        print(data[i]['title'])
-        movies_found = movies_found + 1
-if movies_found == 0:
-    print("No movies found of that name!") """
+def movie_name_finder(name):
+    movies_found = 0
+    print(f"Here are the results for '{name}':")
+    for i in range(len(data)):
+        if data[i]['title'] == name:
+            print(data[i]['title'])
+            movies_found = movies_found + 1
+    if movies_found == 0:
+        print("No movies found of that name!")
+movie_name_finder(1)
 
 #File 6
-genre = input("Search a genre, or several genres.")
-print(f"Here are the results for the genre '{genre}':")
-for i in range(len(data)):
-    if data[i]["genres"] == genre:
-        print(data[i]['title'])
-        genre = genre + 1
+def movie_finder(genre):
+    genre_list = []
+    genre_list.append(genre)
+    found_movies = 0
+    print(f"Here are the results for the genre(s) '{genre_list}':")
+    for i in range(len(data)):
+        if data[i]["genres"] == genre_list:
+            print(data[i]['title'])
+            found_movies += 1
+movie_finder("Drama")
